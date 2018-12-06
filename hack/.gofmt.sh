22eb0c12befb0ed6b194d389ef06a02ed4de89b5
Nick Kubala <nkubala@google.com>
2018-12-06 10:54:52 -0800

    remove go vet from gofmt script

--- hack/.gofmt.sh
+++ hack/.gofmt.sh
@@ -18,12 +18,9 @@ set -e
 
 files=$(find . -name "*.go" | grep -v vendor/ | xargs gofmt -l -s)
 if [[ $files ]]; then
-    echo "Gofmt errors in files: $files"
+    echo "Gofmt errors in files:"
+    echo "$files"
+    diff=$(find . -name "*.go" | grep -v vendor/ | xargs gofmt -d -s)
+    echo "$diff"
     exit 1
-fi
-
-files=$(go vet .)
-if [[ $files ]]; then
-   echo "Go vet errors in files: $files"
-   exit 1
 fi
